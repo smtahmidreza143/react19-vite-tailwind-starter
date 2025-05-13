58f517d0248637e1f5fc73ea11e348125df705ff
smtahmidreza143 <smtahmidreza143@gmail.com>
2025-05-13 17:56:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+
+  return (
+    <>
+      <h1 className="text-5xl text-green-600 font-extrabold text-center">Bismillahir Rahmanir Rahim</h1>
+      <h1 className="text-3xl bg-purple-700 text-white text-center">Alhamdulillah! Successfully Executed!</h1>
+    </>
+  )
+}
+
+export default App
